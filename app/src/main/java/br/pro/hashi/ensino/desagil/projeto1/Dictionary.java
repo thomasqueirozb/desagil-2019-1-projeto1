398813cea54a31c1ae3a97935d5500aaea5e0813
Aronakamoto <aronakamoto@outlook.com>
2019-05-05 12:58:57 -0300

    Dicionario feito falta integracao

---
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/Dictionary.java
@@ -0,0 +1,23 @@
+package br.pro.hashi.ensino.desagil.projeto1;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Dictionary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+    public void sendMessage(View view){
+        Intent intent = new Intent(Dictionary.this, MensagensPredefinidas.class);
+        startActivity(intent);
+    }
+}
