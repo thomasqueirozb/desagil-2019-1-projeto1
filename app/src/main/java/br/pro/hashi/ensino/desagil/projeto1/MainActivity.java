66ff5ec62f1ab3d5c8c582e4f0327e4016a52518
Thomas Queiroz <thomasqueirozb@gmail.com>
2019-05-06 11:01:39 -0300

    SMS permission funcionando com os botões funcionais

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -7,11 +7,12 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.support.v4.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_SEND_SMS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +23,12 @@ public class MainActivity extends AppCompatActivity {
         sendPreDef.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Intent pre = new Intent(MainActivity.this,PreDefMsgs.class);
-                startActivity(pre);
+                if (!hasPermission()){
+                    askPermission();
+                } else {
+                    Intent pre = new Intent(MainActivity.this, PreDefMsgs.class);
+                    startActivity(pre);
+                }
             }
         });
 
@@ -31,15 +36,18 @@ public class MainActivity extends AppCompatActivity {
         sendSMS.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
-                Intent pre = new Intent(MainActivity.this,composeSMS.class);
-                startActivity(pre);
+                if (!hasPermission()){
+                    askPermission();
+                } else {
+                    Intent pre = new Intent(MainActivity.this, composeSMS.class);
+                    startActivity(pre);
+                }
             }
         });
 
-        String[] permissions = new String[]{
-                Manifest.permission.SEND_SMS,
-        };
-        ActivityCompat.requestPermissions(this, permissions, 0);
+        if (!hasPermission()){
+            askPermission();
+        }
     }
 
     @Override
@@ -49,10 +57,21 @@ public class MainActivity extends AppCompatActivity {
         // resposta foi positiva. As respostas estão armazenadas no
         // vetor grantResults, que pode estar vazio se o usuário
         // escolheu simplesmente ignorar o pedido e não responder nada.
-        System.out.println("Callback");
         if (requestCode == 0 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
             // Se foi positiva, podemos iniciar a SMSActivity.
         }
     }
+
+    public boolean hasPermission(){
+        return (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED);
+    }
+
+    public void askPermission(){
+        String[] permissions = new String[]{
+                Manifest.permission.SEND_SMS,
+        };
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_SEND_SMS);
+    }
+
 }
