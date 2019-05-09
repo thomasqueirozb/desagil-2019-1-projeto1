57f06543d3dc6e9d1944b3f8e3129710fa6db231
Thomas Queiroz <thomasqueirozb@gmail.com>
2019-05-09 16:50:41 -0300

    Tentando fix

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/NewPreDefMsg.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/NewPreDefMsg.java
@@ -26,6 +26,7 @@ public class NewPreDefMsg extends AppCompatActivity {
         toast.show();
     }
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
